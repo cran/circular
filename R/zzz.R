997597ab311c147783f933115e2fabf5cf0098f1
Claudio Agostinelli <claudio@unive.it>
2005-05-05 00:00:00 +0000

    version 0.3-2

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg) {
   library.dynam("circular", pkg, lib)
-  cat("This is version 0.3-1 of circular package\n")
+  cat("This is version 0.3-2 of circular package\n")
   cat("Please report any bugs or comments to <Claudio Agostinelli> claudio@unive.it \n")
   cat("The package redefine how function 'var' works \n")
   cat("In particular, (try 'methods(var)') \n notice that 'var.default' is an alias for the original 'var' function \n and that a method for data.frame is available.\n")
